19aeaa6db1de38b489205f9866bdb0c09c43835b
ChristianHopf <cehopf@gmail.com>
2022-03-19 22:31:47 -0400

    Adding bootstrap

---
+++ testapp/src/navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Navbar(){
+    return(
+        <div className="navbar">
+            <h1>HEY</h1>
+            <a href="">Home</a>
+            <a href="">LinkedIn</a>
+            <a href="">GitHub</a>
+        </div>
+    );
+}
+
+export default Navbar;
